cd2b202da55911baa26a0d6f692e0b545f269cba
Mahmood Garibov <mahmood.garibov@gmail.com>
2023-03-06 21:21:19 +0400

    Age changed

--- CA-test/CA-test/Program.cs
+++ CA-test/CA-test/Program.cs
@@ -6,7 +6,7 @@
         {
             string personName = "Mahmood";
             string personLastName = "Garibov";
-            int personAge = 34;
+            int personAge = 38;
             string personFullName = personName + " " + personLastName; //string Concantenation
 
             Console.WriteLine("Salamlar");
