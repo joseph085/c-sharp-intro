40c245116159edfc434bd63050f7933535ff5d99
Mahmood Garibov <mahmood.garibov@gmail.com>
2023-04-04 21:05:45 +0400

    Getting and validation phone number,  pin age, father name, last name, first name completed

--- CA-test/CA-test/Program.cs
+++ CA-test/CA-test/Program.cs
@@ -3,52 +3,52 @@
     internal class Program
     {
         static void Main(string[] args)
-        {   
-            string[] currencies = { "USD", "EUR", "RUB" };
-            decimal[] currencyRates = { 1.70M, 1.82M, 0.02M };
+        {
+            //string[] currencies = { "USD", "EUR", "RUB" };
+            //decimal[] currencyRates = { 1.70M, 1.82M, 0.02M };
 
-            string SHOW_CURRENCY_RATES_COMMAND = "/show-currency-rates";
-            string FIND_CURRENCY_RATE_BY_CODE = "/find-currency-rate-by-cod";
-            string CAULCULATE_AMOUNT_BY_CURRENCY_RATE_BY_CODE = "/calculate-amount-by-currecy-code";
-            string EXIT_COMMAND = "/exit";
+            //string SHOW_CURRENCY_RATES_COMMAND = "/show-currency-rates";
+            //string FIND_CURRENCY_RATE_BY_CODE = "/find-currency-rate-by-cod";
+            //string CAULCULATE_AMOUNT_BY_CURRENCY_RATE_BY_CODE = "/calculate-amount-by-currecy-code";
+            //string EXIT_COMMAND = "/exit";
 
 
-            Console.WriteLine("Available commands : ");
-            Console.WriteLine(SHOW_CURRENCY_RATES_COMMAND);
-            Console.WriteLine(FIND_CURRENCY_RATE_BY_CODE);
-            Console.WriteLine(CAULCULATE_AMOUNT_BY_CURRENCY_RATE_BY_CODE);
-            Console.WriteLine(EXIT_COMMAND);
+            //Console.WriteLine("Available commands : ");
+            //Console.WriteLine(SHOW_CURRENCY_RATES_COMMAND);
+            //Console.WriteLine(FIND_CURRENCY_RATE_BY_CODE);
+            //Console.WriteLine(CAULCULATE_AMOUNT_BY_CURRENCY_RATE_BY_CODE);
+            //Console.WriteLine(EXIT_COMMAND);
 
-            while (true)
-            {
-                Console.WriteLine();
-                Console.Write("Pls enter command : ");
-                string command = Console.ReadLine();
-                Console.WriteLine();
+            //while (true)
+            //{
+            //    Console.WriteLine();
+            //    Console.Write("Pls enter command : ");
+            //    string command = Console.ReadLine();
+            //    Console.WriteLine();
 
-                if (command == SHOW_CURRENCY_RATES_COMMAND)
-                {
-                    ExecuteShowCurrencyRatesCommand(currencyRates, currencies); //METHOD CALL
-                }
-                else if (command == FIND_CURRENCY_RATE_BY_CODE)
-                {
-                    ExecuteFindCurrencyRateByCodeCommand(currencyRates, currencies);
-                }
-                else if (command == CAULCULATE_AMOUNT_BY_CURRENCY_RATE_BY_CODE)
-                {
-                    ExecuteCalculateAmountByCurrencyRateByCode(currencyRates, currencies);
-                }
-                else if (command == EXIT_COMMAND)
-                {
-                    ExecuteExitCommand();
+            //    if (command == SHOW_CURRENCY_RATES_COMMAND)
+            //    {
+            //        ExecuteShowCurrencyRatesCommand(currencyRates, currencies); //METHOD CALL
+            //    }
+            //    else if (command == FIND_CURRENCY_RATE_BY_CODE)
+            //    {
+            //        ExecuteFindCurrencyRateByCodeCommand(currencyRates, currencies);
+            //    }
+            //    else if (command == CAULCULATE_AMOUNT_BY_CURRENCY_RATE_BY_CODE)
+            //    {
+            //        ExecuteCalculateAmountByCurrencyRateByCode(currencyRates, currencies);
+            //    }
+            //    else if (command == EXIT_COMMAND)
+            //    {
+            //        ExecuteExitCommand();
 
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Specified command not found");
-                }
-            }
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Specified command not found");
+            //    }
+            //}
 
         }
 
---
+++ CA-test/HRManagementSystem/Program.cs
@@ -0,0 +1,395 @@
+﻿namespace HRManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetAndValidatePhoneNumber());
+
+
+            //Console.WriteLine(Substring(temp, 5, 2));
+
+            //Console.WriteLine(IsValidPin("123456A"));
+
+            //Console.WriteLine("Person's count : ");
+            //int count = int.Parse(Console.ReadLine());
+
+            //while (count > 0)
+            //{
+            //    string firstName = GetAndValidateFirstName();
+            //    string lastName = GetAndValidateLastName();
+            //    string fatherName = GetAndValidateFatherName();
+            //    int age = GetAndValidateAge();
+            //    string pin = GetAndValidatePin();
+            //    string phoneNumber = GetAndValidatePhoneNumber();
+            //    //string position = GetAndValidatePosition();
+            //    //decimal monthlySalary = GetAndValidateMonthlySalary();
+
+            //    Console.WriteLine($"Məlumat (<{firstName}>, <{lastName}>) Sistemə əlavə olundu");
+
+            //    count--;
+            //}
+        }
+
+
+
+        #region First name
+
+        static string GetAndValidateFirstName()
+        {
+            while (true)
+            {
+                Console.WriteLine("Pls enter first name : ");
+                string firstName = Console.ReadLine()!;
+
+                if (!IsValidFirstName(firstName))
+                {
+                    Console.WriteLine("Some information is not correnct");
+                }
+            }
+        }
+        static bool IsValidFirstName(string firstName)
+        {
+            int MIN_LENGTH = 2;
+            int MAX_LENGTH = 20;
+
+            return IsValidName(firstName, MIN_LENGTH, MAX_LENGTH);
+        }
+
+        #endregion
+
+        #region Last name
+
+        static string GetAndValidateLastName()
+        {
+            while (true)
+            {
+                Console.WriteLine("Pls enter last name : ");
+                string firstName = Console.ReadLine()!;
+
+                if (!IsValidLastName(firstName))
+                {
+                    Console.WriteLine("Some information is not correnct");
+                }
+            }
+        }
+        static bool IsValidLastName(string lastName)
+        {
+            int MIN_LENGTH = 2;
+            int MAX_LENGTH = 30;
+
+            return IsValidName(lastName, MIN_LENGTH, MAX_LENGTH);
+        }
+
+        #endregion
+
+        #region Father name
+
+        static string GetAndValidateFatherName()
+        {
+            while (true)
+            {
+                Console.WriteLine("Pls enter father name : ");
+                string fatherName = Console.ReadLine()!;
+
+                if (!IsValidLastName(fatherName))
+                {
+                    Console.WriteLine("Some information is not correnct");
+                }
+            }
+        }
+        static bool IsValidFatherName(string fatherName)
+        {
+            int MIN_LENGTH = 2;
+            int MAX_LENGTH = 45;
+
+            return IsValidName(fatherName, MIN_LENGTH, MAX_LENGTH);
+        }
+
+        #endregion
+
+        #region Age
+
+        static int GetAndValidateAge()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Pls enter age : ");
+                int age = int.Parse(Console.ReadLine()!);
+
+                //Early return 
+                if (IsLengthBetween(age, 18, 65))
+                {
+                    return age;
+                }
+
+                Console.WriteLine("Some information is not correnct");
+            }
+        }
+
+
+        #endregion
+
+        #region PIN
+
+        static string GetAndValidatePin()
+        {
+            while (true)
+            {
+                Console.WriteLine("Pls enter pin : ");
+                string pin = Console.ReadLine()!;
+
+                if (!IsValidPin(pin))
+                {
+                    Console.WriteLine("Some information is not correnct");
+                }
+            }
+        }
+        static bool IsValidPin(string pin)
+        {
+            int PIN_LENGTH = 7;
+
+            if (!IsExactLength(pin, PIN_LENGTH))
+            {
+                return false;
+            }
+
+            //71ERAJ
+
+
+
+
+            bool isUpperLetter = false;
+            bool isDigit = false;
+
+            foreach (char character in pin)
+            {
+                if (IsUpperLetter(character))
+                {
+                    isUpperLetter = true;
+                }
+                else if (IsDigit(character))
+                {
+                    isDigit = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return isUpperLetter && isDigit;
+        }
+
+        #endregion
+
+        #region Phone number
+
+        static string GetAndValidatePhoneNumber()
+        {
+            while (true)
+            {
+                Console.WriteLine("Pls enter phone number : ");
+                string phoneNumber = Console.ReadLine()!;
+
+                if (IsValidPhoneNumber(phoneNumber))
+                {
+                    return phoneNumber;
+                }
+
+                Console.WriteLine("Some information is not correnct");
+            }
+        }
+        static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            string[] prefixes = { "+99450", "+99455", "+99470" };
+
+            int PHONE_NUMBER_LENGTH = 13;
+
+            if (!IsExactLength(phoneNumber, PHONE_NUMBER_LENGTH))
+            {
+                return false;
+            }
+
+            string portion = SubstringFromEnd(phoneNumber, 7);
+
+            if (!IsDigit(portion))
+            {
+                return false;
+            }
+
+            //+994503144847
+
+            foreach (string prefix in prefixes)
+            {
+                if (IsStartsWith(phoneNumber, prefix))
+                {
+                    return true;
+                }
+            }
+
+            return false; ;
+        }
+
+        #endregion
+
+
+        #region Common
+
+        static bool IsValidName(string name, int minLength, int maxLenght)
+        {
+            if (IsLengthBetween(name, minLength, maxLenght))
+            {
+                return false;
+            }
+
+            char firstLetter = name[0];
+
+            if (!IsUpperLetter(firstLetter))
+            {
+                return false;
+            }
+
+            for (int i = 1; i < name.Length; i++)
+            {
+                if (!IsUpperLetter(name[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region Utility
+
+        public static bool IsStartsWith(string text, string startText)
+        {
+            if (startText.Length > text.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < startText.Length; i++)
+            {
+                if (text[i] != startText[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static string Substring(string text, int startIdx, int endIdx)
+        {
+            string subString = "";
+
+            for (int i = startIdx; i <= endIdx; i++)
+            {
+                subString += text[i];
+            }
+
+            return subString;
+        }
+        public static string SubstringFromEnd(string text, int length)
+        {
+            if (text.Length <= length || length < 0)
+            {
+                return default;
+            }
+
+            string subString = "";
+
+            for (int i = text.Length - 1; i >= text.Length - length; i--)
+            {
+                subString += text[i];
+            }
+
+            return Reverse(subString);
+        }
+
+        public static string Reverse(string text)
+        {
+            string reversed = "";
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversed += text[i];
+            }
+
+            return reversed;
+        }
+
+        public static bool IsLengthBetween(string text, int min, int max)
+        {
+            return text.Length > min && text.Length < max;
+        }
+
+        //Method overloading (static polymorphism)
+        public static bool IsLengthBetween(int number, int min, int max)
+        {
+            return number > min && number < max;
+        }
+        public static bool IsUpperLetter(char letter)
+        {
+            char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            foreach (char uppercaseLetter in uppercaseLetters) //while LOOP
+            {
+                if (uppercaseLetter == letter)
+                {
+                    return true;
+                }
+            }
+
+            //for (int i = 0; i < uppercaseLetters.Length; i++)
+            //{
+            //    if (uppercaseLetters[i] == lette)
+            //    {
+            //        return true;
+            //    }
+            //}
+
+            return false;
+        }
+
+
+        public static bool IsDigit(string text)
+        {
+            foreach (char characted in text)
+            {
+                if (!IsDigit(characted))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public static bool IsDigit(char digit)
+        {
+            char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            foreach (char number in numbers) //Compiled to while LOOP in IL
+            {
+                if (digit == number)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public static bool IsExactLength(string text, int length)
+        {
+            return text.Length == length;
+        }
+
+        #endregion
+    }
+}
