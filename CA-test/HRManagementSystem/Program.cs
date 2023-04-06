9d682f4b5bdfe83418a0141773c92506d55b4bed
Mahmood Garibov <mahmood.garibov@gmail.com>
2023-04-06 21:51:48 +0400

    Our application structure updated

--- CA-test/HRManagementSystem/Program.cs
+++ CA-test/HRManagementSystem/Program.cs
@@ -1,91 +1,47 @@
-﻿namespace HRManagementSystem
+﻿using System;
+
+namespace HRManagementSystem
 {
     //custom type
-    public class Person
+    public class Student
     {
         //fields (data)
         public string _firstName;
         public string _lastName;
         public string _fatherName;
-        //public int _age;
-        //public string _pin;
-        //public string _phoneNumber;
-        //public string _position;
-        //public decimal _montlySalary;
+        public int _age;
     }
 
-    public class Program
+    public class AddStudentCommand
     {
-        static void Main(string[] args)
+        public Utility _utility = new Utility();
+
+        public void Handle(List<Student> argStudents)
         {
-            Console.WriteLine("Person's count : ");
-            int count = int.Parse(Console.ReadLine()!);
-            Person[] persons = new Person[count];
+            string firstName = GetAndValidateFirstName();
+            string lastName = GetAndValidateLastName();
+            string fatherName = GetAndValidateFatherName();
+            int age = GetAndValidateAge();
 
-            int i = 0;
-
-            while (i < count)
+            Student human = new Student
             {
-                string firstName = GetAndValidateFirstName();
-                string lastName = GetAndValidateLastName();
-                string fatherName = GetAndValidateFatherName();
-                //int age = GetAndValidateAge();
-                //string pin = GetAndValidatePin();
-                //string phoneNumber = GetAndValidatePhoneNumber();
-                //string position = GetAndValidatePosition();
-                //decimal monthlySalary = GetAndValidateMonthlySalary();
+                _firstName = firstName,
+                _lastName = lastName,
+                _fatherName = fatherName,
+                _age = age,
+            };
 
-                Person human = new Person
-                {
-                    _firstName = firstName,
-                    _lastName = lastName,
-                    _fatherName = fatherName,
-                    //_age = age,
-                    //_pin = pin,
-                    //_phoneNumber = phoneNumber,
-                    //_position = position,
-                    //_montlySalary = monthlySalary
-                };
+            argStudents.Add(human);
 
-                persons[i] = human;
+            //argStudents[lastStudentIdx] = human;
+            //lastStudentIdx = lastStudentIdx + 1;
 
-                //firstNames[i] = firstName;
-                //lastNames[i] = lastName;
-                //fatherNames[i] = fatherName;
-                //ages[i] = age;
-                //pins[i] = pin;
-                //phoneNumbers[i] = phoneNumber;
-                //positions[i] = position;
-                //monthlySalaries[i] = monthlySalary;
-
-                Console.WriteLine($"Məlumat Sistemə əlavə olundu");
-
-                i++;
-            }
-
-            PrintPersonsInfo(persons);
-        }
-
-        public static void PrintPersonsInfo(Person[] people)
-        {
-            for (int k = 0; k < people.Length; k++)
-            {
-                Console.WriteLine($"First name : {people[k]._firstName}");
-                Console.WriteLine($"Last name : {people[k]._lastName}");
-                Console.WriteLine($"Father name : {people[k]._fatherName}");
-
-                //Console.WriteLine($"Age : {persons[k]}");
-                //Console.WriteLine($"Pin : {persons[k]}");
-                //Console.WriteLine($"Phone number : {persons[k]}");
-                //Console.WriteLine($"Position : {persons[k]}");
-                //Console.WriteLine($"Monthly salary : {persons[k]}");
-                //Console.WriteLine();
-            }
+            Console.WriteLine($"Məlumat Sistemə əlavə olundu");
         }
 
         #region First name
 
-        static string GetAndValidateFirstName()
+        string GetAndValidateFirstName()
         {
             while (true)
             {
@@ -98,7 +54,7 @@
                 Console.WriteLine("Some information is not correnct");
             }
         }
-        static bool IsValidFirstName(string firstName)
+        bool IsValidFirstName(string firstName)
         {
             int MIN_LENGTH = 2;
             int MAX_LENGTH = 20;
@@ -110,7 +66,7 @@
 
         #region Last name
 
-        static string GetAndValidateLastName()
+        string GetAndValidateLastName()
         {
             while (true)
             {
@@ -123,7 +79,7 @@
                 Console.WriteLine("Some information is not correnct");
             }
         }
-        static bool IsValidLastName(string lastName)
+        bool IsValidLastName(string lastName)
         {
             int MIN_LENGTH = 2;
             int MAX_LENGTH = 30;
@@ -135,7 +91,7 @@
 
         #region Father name
 
-        static string GetAndValidateFatherName()
+        string GetAndValidateFatherName()
         {
             while (true)
             {
@@ -148,7 +104,7 @@
                 Console.WriteLine("Some information is not correnct");
             }
         }
-        static bool IsValidFatherName(string fatherName)
+        bool IsValidFatherName(string fatherName)
         {
             int MIN_LENGTH = 2;
             int MAX_LENGTH = 45;
@@ -160,190 +116,20 @@
 
         #region Age
 
-        static int GetAndValidateAge()
+        int GetAndValidateAge()
         {
             while (true)
             {
                 Console.WriteLine("Pls enter age : ");
-                var isParsable = TryParse(Console.ReadLine()!, out int age);
+                var isParsable = _utility.TryParse(Console.ReadLine()!, out int age);
                 if (!isParsable)
                 {
                     continue;
                 }
 
                 //Early return 
-                if (IsLengthBetween(age, 18, 65))
+                if (_utility.IsLengthBetween(age, 18, 65))
                     return age;
-
-                Console.WriteLine("Some information is not correnct");
-            }
-        }
-
-        #endregion
-
-        #region PIN
-
-        static string GetAndValidatePin()
-        {
-            while (true)
-            {
-                Console.WriteLine("Pls enter pin : ");
-                string pin = Console.ReadLine()!;
-
-                if (IsValidPin(pin))
-                    return pin;
-
-                Console.WriteLine("Some information is not correnct");
-            }
-        }
-        static bool IsValidPin(string pin)
-        {
-            int PIN_LENGTH = 7;
-
-            if (!IsExactLength(pin, PIN_LENGTH))
-            {
-                return false;
-            }
-
-            bool isUpperLetter = false;
-            bool isDigit = false;
-
-            foreach (char character in pin)
-            {
-                if (IsUpperLetter(character))
-                {
-                    isUpperLetter = true;
-                }
-                else if (IsDigit(character))
-                {
-                    isDigit = true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-
-            return isUpperLetter && isDigit;
-        }
-
-        #endregion
-
-        #region Phone number
-
-        static string GetAndValidatePhoneNumber()
-        {
-            while (true)
-            {
-                Console.WriteLine("Pls enter phone number : ");
-                string phoneNumber = Console.ReadLine()!;
-
-                if (IsValidPhoneNumber(phoneNumber))
-                    return phoneNumber;
-
-                Console.WriteLine("Some information is not correnct");
-            }
-        }
-        static bool IsValidPhoneNumber(string phoneNumber)
-        {
-            string[] prefixes = { "+99450", "+99455", "+99470" };
-
-            int PHONE_NUMBER_LENGTH = 13;
-
-            if (!IsExactLength(phoneNumber, PHONE_NUMBER_LENGTH))
-            {
-                return false;
-            }
-
-            string phoneNumberPrefix = null;
-
-            foreach (string prefix in prefixes)
-            {
-                if (IsStartsWith(phoneNumber, prefix))
-                {
-                    phoneNumberPrefix = prefix;
-                }
-            }
-
-            //+99450
-
-            if (phoneNumberPrefix == null)
-                return false;
-
-            string substring = Substring(phoneNumber, phoneNumberPrefix.Length, phoneNumber.Length - 1);
-
-            if (!IsDigit(substring))
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        #endregion
-
-        #region Position
-
-        static string GetAndValidatePosition()
-        {
-            while (true)
-            {
-                Console.WriteLine("Pls enter position : ");
-                string position = Console.ReadLine()!;
-
-                if (IsValidPosition(position))
-                    return position;
-
-
-                Console.WriteLine("Some information is not correnct");
-            }
-        }
-        static bool IsValidPosition(string position)
-        {
-            //if (position == "HR")
-            //{
-            //    return true;
-            //}
-            //else if(position == "Audit")
-            //{
-            //    return true;
-            //}
-            //else if (position == "Engineer ")
-            //{
-            //    return true;
-            //}
-            //else
-            //{
-            //    return false;
-            //}
-
-
-            switch (position)
-            {
-                case "HR":
-                case "Audit":
-                case "Engineer":
-                    return true;
-
-                default:
-                    return false;
-            }
-        }
-
-        #endregion
-
-        #region Monthly salary
-
-        static decimal GetAndValidateMonthlySalary()
-        {
-            while (true)
-            {
-                Console.WriteLine("Pls enter monthly salary : ");
-                decimal amount = decimal.Parse(Console.ReadLine()!);
-
-                //Early return 
-                if (IsLengthBetween(amount, 1500, 5000))
-                    return amount;
 
                 Console.WriteLine("Some information is not correnct");
             }
@@ -353,23 +139,23 @@
 
         #region Common
 
-        static bool IsValidName(string name, int minLength, int maxLenght)
+        bool IsValidName(string name, int minLength, int maxLenght)
         {
-            if (!IsLengthBetween(name, minLength, maxLenght))
+            if (!_utility.IsLengthBetween(name, minLength, maxLenght))
             {
                 return false;
             }
 
             char firstLetter = name[0];
 
-            if (!IsUpperLetter(firstLetter))
+            if (!_utility.IsUpperLetter(firstLetter))
             {
                 return false;
             }
 
             for (int i = 1; i < name.Length; i++)
             {
-                if (IsUpperLetter(name[i]))
+                if (_utility.IsUpperLetter(name[i]))
                 {
                     return false;
                 }
@@ -379,10 +165,58 @@
         }
 
         #endregion
+    }
 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Sutndet's count : ");
+            //int count = int.Parse(Console.ReadLine()!);
+            //Student[] students = new Student[count];
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+                Console.WriteLine("Pls enter command : ");
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "/add-student":
+                        AddStudentCommand addStudentCommand = new AddStudentCommand();
+                        addStudentCommand.Handle(students);
+                        break;
+
+                    case "/exit":
+                        break;
+                }
+            }
+        }
+
+        //public static void PrintPersonsInfo(Student[] people)
+        //{
+        //    for (int k = 0; k < people.Length; k++)
+        //    {
+        //        Console.WriteLine($"First name : {people[k]._firstName}");
+        //        Console.WriteLine($"Last name : {people[k]._lastName}");
+        //        Console.WriteLine($"Father name : {people[k]._fatherName}");
+
+        //        //Console.WriteLine($"Age : {persons[k]}");
+        //        //Console.WriteLine($"Pin : {persons[k]}");
+        //        //Console.WriteLine($"Phone number : {persons[k]}");
+        //        //Console.WriteLine($"Position : {persons[k]}");
+        //        //Console.WriteLine($"Monthly salary : {persons[k]}");
+        //        //Console.WriteLine();
+        //    }
+        //}
+    }
+
+    public class Utility
+    {
         #region Utility
 
-        public static bool TryParse(string text, out int number)
+        public bool TryParse(string text, out int number)
         {
             try
             {
@@ -395,7 +229,7 @@
                 return false;
             }
         }
-        public static bool IsStartsWith(string text, string startText)
+        public bool IsStartsWith(string text, string startText)
         {
             if (startText.Length > text.Length)
             {
@@ -412,7 +246,7 @@
 
             return true;
         }
-        public static string Substring(string text, int startIdx, int endIdx)
+        public string Substring(string text, int startIdx, int endIdx)
         {
             string subString = "";
 
@@ -423,7 +257,7 @@
 
             return subString;
         }
-        public static string SubstringFromEnd(string text, int length)
+        public string SubstringFromEnd(string text, int length)
         {
             if (text.Length <= length || length < 0)
             {
@@ -440,7 +274,7 @@
             return Reverse(subString);
         }
 
-        public static string Reverse(string text)
+        public string Reverse(string text)
         {
             string reversed = "";
 
@@ -452,21 +286,21 @@
             return reversed;
         }
 
-        public static bool IsLengthBetween(decimal number, decimal min, decimal max)
+        public bool IsLengthBetween(decimal number, decimal min, decimal max)
         {
             return number > min && number < max;
         }
-        public static bool IsLengthBetween(string text, int min, int max)
+        public bool IsLengthBetween(string text, int min, int max)
         {
             return text.Length > min && text.Length < max;
         }
 
-        //Method overloading (static polymorphism)
-        public static bool IsLengthBetween(int number, int min, int max)
+        //Method overloading ( polymorphism)
+        public bool IsLengthBetween(int number, int min, int max)
         {
             return number > min && number < max;
         }
-        public static bool IsUpperLetter(char letter)
+        public bool IsUpperLetter(char letter)
         {
             char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                                         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
@@ -491,7 +325,7 @@
         }
 
 
-        public static bool IsDigit(string text)
+        public bool IsDigit(string text)
         {
             foreach (char characted in text)
             {
@@ -503,7 +337,7 @@
 
             return true;
         }
-        public static bool IsDigit(char digit)
+        public bool IsDigit(char digit)
         {
             char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
@@ -517,7 +351,7 @@
 
             return false;
         }
-        public static bool IsExactLength(string text, int length)
+        public bool IsExactLength(string text, int length)
         {
             return text.Length == length;
         }
